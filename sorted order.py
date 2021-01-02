28f6b596d92051f5cd105d76bcddf4a07ef32673
malvina-s <stammalv@gmail.com>
2021-01-02 21:33:46 +0200

    Create Day 18

---
+++ sorted order.py
@@ -0,0 +1,19 @@
+#sorted order
+
+
+try:
+    x = int(input('Enter an integer (0 to to exit): '))
+except ValueError:
+    print ('Your entry was not an integer. Exiting...')
+num_list = []
+
+while x != 0:
+    num_list.append(x)
+    try:
+        x = int(input('Enter an integer (0 to to exit): '))
+    except ValueError:
+        print ('Your entry was not an integer. Exiting...')
+        
+num_list = sorted(num_list)
+
+print (num_list)
